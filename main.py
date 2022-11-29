70dece9d1ed5d51932a7b3b2c8add9504cf66728
Yiliang Wan <yiliangw@hotmail.com>
2022-11-29 10:53:37 +0800

    Improve output format

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ SUB_P = 0.01
 DEL_P = 0.01
 INS_P = 0.01
 
+
+def align_sequence(sequence, markers):
+    markers = sorted([(p, m) for p, m in markers.items()], key=(lambda t: t[0]))
+    out = ''
+    lp = 0
+    for p, m in markers:
+        out += sequence[lp:p]
+        out += '-' * len(m)
+        lp = p
+    out += sequence[lp:]
+    return out
+
+
 def main():
     # random.seed()
     symbols.init('AGCT')
     
     gold = ''.join(random.choices(symbols.all(), k=116))
 
-    # markers = {int(len(gold)/2): 'AAAA'}
-    markers = {}
+    markers = {int(len(gold)*1/3): 'AAA', int(len(gold)*2/3): 'AAA'}
+    # markers = {}
     encoded = marker_code.encode(gold, markers)
 
     samples = gen_noisy_samples(encoded, 6, SUB_P, DEL_P, INS_P)
@@ -22,7 +35,7 @@ def main():
 
     decoded_marker, decoded = marker_code.decode(samples, len(gold), markers, SUB_P, DEL_P, INS_P)
 
-    print("withmarker:\t{}\nencoded:\t{}\ngold:\t\t{}\ndecoded:\t{}".format( decoded_marker, encoded, gold, decoded))
+    print("withmarker:\t{}\nencoded:\t{}\ngold:\t\t{}\ndecoded:\t{}".format(decoded_marker, encoded, align_sequence(gold, markers), align_sequence(decoded, markers)))
     print("decoded equals to gold: {}".format(decoded == gold))
     return
 
